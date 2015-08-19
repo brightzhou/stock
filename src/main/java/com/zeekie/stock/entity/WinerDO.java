f663b0f877593903f818835ae03093cc44aef907
zeekie <yanhuodig@sina.com>
2015-08-19 14:10:04 +0800

    ss

---
+++ src/main/java/com/zeekie/stock/entity/WinerDO.java
@@ -0,0 +1,25 @@
+package com.zeekie.stock.entity;
+
+public class WinerDO {
+
+	private String nickname;
+
+	private String profit;
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getProfit() {
+		return profit;
+	}
+
+	public void setProfit(String profit) {
+		this.profit = profit;
+	}
+
+}
